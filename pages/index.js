b3bcfc2a45be93ed9d2e8009559a5e10af2a3ccd
Alien <alina.selitskaya1278@gmail.com>
2023-03-19 05:52:43 +0300

    feature: added cards from array

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+//user information editing
+
 const popup = document.querySelector(".popup");
 const popupForm = document.querySelector(".popup__form");
 
@@ -30,3 +32,46 @@ function edit(e) {
 popupOpen.addEventListener("click", open);
 popupClose.addEventListener("click", close);
 popupForm.addEventListener("submit", edit);
+
+//initialCards
+const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
+
+function initialCardItems() {
+  for (i = 0; i < initialCards.length; i++) {
+    const cardItem = document.getElementById("cardItem").content;
+    const cardNewItem = cardItem.querySelector(".card").cloneNode(true);
+
+    cardNewItem.querySelector(".card__title").textContent =
+      initialCards[i].name;
+    cardNewItem.querySelector(".card__image").src = initialCards[i].link;
+
+    document.querySelector(".cards").append(cardNewItem);
+  }
+}
+
+initialCardItems();
