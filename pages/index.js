540e87ca2f03b27ebcb4de4d99b3ccc54bba98fc
Alien <alina.selitskaya1278@gmail.com>
2023-03-22 03:24:17 +0300

    feature: add the ability to add a card and like a photo

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,78 @@
-//user information editing
+const popupAdd = document.querySelector(".popup_add");
+const popupEdit = document.querySelector(".popup_edit");
 
-const popup = document.querySelector(".popup");
-const popupForm = document.querySelector(".popup__form");
+const popupFormEdit = popupEdit.querySelector(".popup__form_edit");
+const popupFormAdd = popupAdd.querySelector(".popup__form_add");
 
-const popupOpen = document.querySelector(".profile__edit");
-const popupClose = document.querySelector(".popup__close");
+const popupOpenEdit = document.querySelector(".profile__edit");
+const popupCloseEdit = popupEdit.querySelector(".popup__close_edit");
+const popupOpenAdd = document.querySelector(".profile__add-card");
+const popupCloseAdd = popupAdd.querySelector(".popup__close_add");
 
 const userName = document.getElementById("username-input");
 const userDescription = document.getElementById("description-input");
+const nameCard = document.getElementById("name-card-input");
+const urlCard = document.getElementById("url-card-input");
 
 const profileName = document.querySelector(".profile__name");
 const profileDescription = document.querySelector(".profile__description");
+const cardName = document.querySelector(".card__title");
+const cardUrl = document.querySelector(".card__image");
 
-function open() {
+//func open close modal window
+function open(popup) {
   popup.classList.add("popup_opened");
-  userName.value = profileName.textContent;
-  userDescription.value = profileDescription.textContent;
 }
-
-function close() {
+function close(popup) {
   popup.classList.remove("popup_opened");
 }
-
+//func edit profile info
 function edit(e) {
   e.preventDefault();
   profileName.textContent = userName.value;
   profileDescription.textContent = userDescription.value;
-  close();
+  close(popupEdit);
+}
+//func add new card
+function addCard(e) {
+  e.preventDefault();
+  card(nameCard.value, urlCard.value);
+  close(popupAdd);
+}
+//func create card
+function card(name, imageSrc) {
+  const cardItem = document.getElementById("cardItem").content;
+  const cardNewItem = cardItem.querySelector(".card").cloneNode(true);
+
+  cardNewItem.querySelector(".card__title").textContent = name;
+  cardNewItem.querySelector(".card__image").src = imageSrc;
+
+  const like = cardNewItem.querySelector(".card__like");
+  like.addEventListener("click", likeCard);
+
+  document.querySelector(".cards").append(cardNewItem);
 }
 
-popupOpen.addEventListener("click", open);
-popupClose.addEventListener("click", close);
-popupForm.addEventListener("submit", edit);
+// edit listeners
+popupOpenEdit.addEventListener("click", () => {
+  open(popupEdit);
 
-//initialCards
+  userName.value = profileName.textContent;
+  userDescription.value = profileDescription.textContent;
+});
+popupCloseEdit.addEventListener("click", () => close(popupEdit));
+popupFormEdit.addEventListener("submit", edit);
+// add card listeners
+popupOpenAdd.addEventListener("click", () => {
+  open(popupAdd);
+
+  nameCard.value = "";
+  urlCard.value = "";
+});
+popupCloseAdd.addEventListener("click", () => close(popupAdd));
+popupFormAdd.addEventListener("submit", addCard);
+
+//initialCards array
 const initialCards = [
   {
     name: "Архыз",
@@ -60,18 +99,14 @@ const initialCards = [
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
   },
 ];
-
+//func add initial Cards from array
 function initialCardItems() {
   for (i = 0; i < initialCards.length; i++) {
-    const cardItem = document.getElementById("cardItem").content;
-    const cardNewItem = cardItem.querySelector(".card").cloneNode(true);
-
-    cardNewItem.querySelector(".card__title").textContent =
-      initialCards[i].name;
-    cardNewItem.querySelector(".card__image").src = initialCards[i].link;
-
-    document.querySelector(".cards").append(cardNewItem);
+    card(initialCards[i].name, initialCards[i].link);
   }
 }
-
 initialCardItems();
+//func toggle like button
+function likeCard(e) {
+  e.target.classList.toggle("card__like_active");
+}
